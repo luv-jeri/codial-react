04e4339a413b767070a38dc25dcdae74ad9d0cd2
rutvij-fsd <rutvijpatel414@gmail.com>
2022-10-08 22:27:29 +0530

    Fetch Posts from given post api&updating the store

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './Components/App';
-import { configureStore } from './Store/index';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+
+import "./index.css";
+import App from "./Components/App";
+import { configureStore } from "./Store/index";
 
 const store = configureStore();
-console.log('Store',store.getState());
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
-
-
