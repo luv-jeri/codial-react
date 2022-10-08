39513eb6cf5ae17db4e2bb1e8f2498a9d84522f2
rutvij-fsd <rutvijpatel414@gmail.com>
2022-10-09 00:23:08 +0530

    Add prop-types for type validation

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,32 +1,32 @@
-import React, { Component } from 'react'
-import {connect} from 'react-redux';
-import {PostsList} from './';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { PostsList } from './';
+import PropTypes from 'prop-types';
 
-
-import {fetchPosts} from '../Actions/Posts';
-
+import { fetchPosts } from '../Actions/Posts';
 
 class App extends Component {
-
-  componentDidMount() { 
-    this.props.dispatch(fetchPosts()) 
+  componentDidMount() {
+    this.props.dispatch(fetchPosts());
   }
 
   render() {
-    console.log('props',this.props)
+    console.log('props', this.props);
     return (
       <div>
         <PostsList posts={this.props.posts} />
       </div>
-    )
+    );
   }
 }
 
-
-
-function mapStateToProps(state){
-  return{
-    posts : state.posts
-  }
+function mapStateToProps(state) {
+  return {
+    posts: state.posts,
+  };
 }
+
+App.propTypes = {
+  posts: PropTypes.array.isRequired,
+};
 export default connect(mapStateToProps)(App);
