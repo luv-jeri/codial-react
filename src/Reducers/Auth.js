7cd9bb3e054ef664e30c4d0f447f08eb0a45fc3e
rutvij-fsd <rutvijpatel414@gmail.com>
2022-10-10 22:14:34 +0530

    Add Action type and reducer for Login

---
+++ src/Reducers/Auth.js
@@ -0,0 +1,34 @@
+import { LOGIN_START, LOGIN_SUCCESS } from "../Actions/Action_Types";
+
+const initialAuthState = {
+    user:{},
+    error:null,
+    isLoggedIn:false,
+    inProgress:false,
+};
+
+export default function auth(state=initialAuthState,action){
+    switch(action.type){
+        case LOGIN_START:
+            return{
+                ...state,
+                inProgress:true,
+            }
+        case LOGIN_SUCCESS:
+            return{
+                ...state,
+                user: action.user,
+                isLoggedIn: true,
+                inProgress:false,
+                error:null,
+            }
+        case LOGIN_FAILED:
+            return{
+                ...state,
+                error: action.error,
+                inProgress:false,
+            }
+        default:
+            return state;
+    }
+}
